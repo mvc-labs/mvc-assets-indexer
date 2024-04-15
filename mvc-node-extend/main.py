cdb6bb36e9e09c9ed756e1d4b84db7adb7ab7797
georgeCyber3 <george@cyber3.space>
2024-04-15 15:39:38 +0800

    feat: remove tx index, remove zmq listen

--- mvc-node-extend/main.py
+++ mvc-node-extend/main.py
@@ -5,23 +5,25 @@ import time
 import queue
 import hashlib
 import threading
-from io import BytesIO
 from datetime import datetime
 from base64 import encodebytes
+from json import JSONDecodeError
 from typing import BinaryIO, Optional, Literal, Union, Annotated
 
-import zmq
 import plyvel
-import uvicorn
 import requests
+import uvicorn
 from dotenv import load_dotenv
 from mvclib import Transaction
 from mvclib.hash import hash256
 from mvclib.transaction import TransactionBytesIO
 from fastapi import FastAPI, HTTPException, Depends, status
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
-from requests import JSONDecodeError
 from starlette.responses import Response
+
+
+def log(*args, **kwargs):
+    print(datetime.now(), *args, **kwargs, flush=True)
 
 
 def merkle(hash_list):
@@ -38,7 +40,7 @@ def merkle(hash_list):
 def _hash_merkle_node(a, b):
     a1 = bytes.fromhex(a)[::-1]
     b1 = bytes.fromhex(b)[::-1]
-    h = hashlib.sha256(hashlib.sha256(a1+b1).digest()).digest()
+    h = hashlib.sha256(hashlib.sha256(a1 + b1).digest()).digest()
     return h[::-1].hex()
 
 
@@ -49,9 +51,7 @@ app = FastAPI()
 security = HTTPBasic()
 CONFIG_FILE = os.environ['CONFIG_FILE']
 DB_LEVEL_INDEXER_PATH = os.environ['DB_LEVEL_INDEXER_PATH']
-DB_LEVEL_MEMPOOL_PATH = os.environ['DB_LEVEL_MEMPOOL_PATH']
 NODE_DATA_PATH = os.environ['NODE_DATA_PATH']
-ZMQ_SERVER = os.environ['ZMQ_SERVER']
 RPC_HOST = os.environ['RPC_HOST']
 RPC_PORT = os.environ['RPC_PORT']
 RPC_USER = os.environ['RPC_USER']
@@ -64,26 +64,39 @@ HEADERS = {
 RPC_URL = f'http://{RPC_HOST}:{RPC_PORT}'
 memory_queue = queue.Queue()
 db_level_indexer = plyvel.DB(DB_LEVEL_INDEXER_PATH, create_if_missing=True)
-db_level_mempool = plyvel.DB(DB_LEVEL_MEMPOOL_PATH, create_if_missing=True)
 
 if not os.path.exists(f'{NODE_DATA_PATH}/blocks'):
     print('Please config right NODE_DATA_PATH:', f'{NODE_DATA_PATH}')
     exit()
 
-def mvc_node_is_safe():
+
+def get_blk_dat_file(blk_number: int):
+    return f'{NODE_DATA_PATH}/blocks/blk{str(blk_number).zfill(5)}.dat'
+
+
+def check_rpc_config():
     data = {
         'jsonrpc': "1.0",
         'id': int(time.time()),
         'method': "getsafemodeinfo",
     }
-    resp = requests.post(RPC_URL, json=data,headers=HEADERS)
-    enable = True
+    resp = requests.post(RPC_URL, json=data, headers=HEADERS)
     try:
-        enable = resp.json()['result']['safemodeenabled']
+        log(resp.json())
     except JSONDecodeError as _:
-        log('_', _)
-        pass
-    return enable
+        log('rpc config error, please set correct rpc username and password', _)
+        exit()
+
+
+BLOCK_DATA_0 = get_blk_dat_file(0)
+MAGIC_BYTES = b''
+if os.path.exists(BLOCK_DATA_0):
+    with open(BLOCK_DATA_0, 'rb') as dat_f:
+        MAGIC_BYTES = dat_f.read(4)
+if MAGIC_BYTES.__len__() == 0:
+    print('MAGIC_BYTES read error')
+    exit()
+check_rpc_config()
 
 
 class BlockParseError(Exception):
@@ -153,7 +166,7 @@ class DatParse:
         self.last_magic_read_position = self._io.tell()
         for i in range(5):
             magic = self._io.read(4)
-            if magic == b'i\xc3Z\xa5':
+            if magic == MAGIC_BYTES:
                 return magic
             else:
                 self.read_size()
@@ -175,44 +188,9 @@ class DatParse:
         return self._io.tell()
 
 
-class ZmqThread(threading.Thread):
-
-    def __init__(self, url):
-        threading.Thread .__init__(self, daemon=True)
-        self.ctx = zmq.Context()
-        self.sock = self.ctx.socket(zmq.SUB)
-        # only listen raw tx
-        self.sock.setsockopt(zmq.SUBSCRIBE, b"rawtx")
-        # set zmq keep alive
-        self.sock.setsockopt(zmq.TCP_KEEPALIVE, 1)
-        self.sock.setsockopt(zmq.TCP_KEEPALIVE_IDLE, 60)
-        self.sock.setsockopt(zmq.TCP_KEEPALIVE_INTVL, 1)
-        self.sock.connect(url)
-        self.last_check_safe_time = int(time.time())
-        self.check_safe_interval = 5
-        self.is_safe_mode = True
-
-    def run(self) -> None:
-        while True:
-            msg = self.sock.recv_multipart()
-            if time.time() - self.last_check_safe_time > self.check_safe_interval:
-                self.is_safe_mode = mvc_node_is_safe()
-                self.last_check_safe_time = int(time.time())
-            tx_bytes = msg[1]
-            # cal txid
-            tx = Transaction.from_hex(tx_bytes)
-            # upload tx bytes
-            if not self.is_safe_mode:
-                # not in safe mode
-                memory_queue.put({
-                    'txid': tx.txid(),
-                    'tx_binary': tx_bytes
-                })
-
-
 class ApiThread(threading.Thread):
     def __init__(self):
-        threading.Thread .__init__(self, daemon=True)
+        threading.Thread.__init__(self, daemon=True)
 
     def run(self):
         uvicorn.run(
@@ -221,10 +199,6 @@ class ApiThread(threading.Thread):
             port=8000,
             loop='uvloop'
         )
-
-
-def log(*args, **kwargs):
-    print(datetime.now(), *args, **kwargs, flush=True)
 
 
 def get_config():
@@ -244,34 +218,23 @@ def save_config():
         f.write(json.dumps(config, indent=2))
 
 
-def upload_block_txs(blk_number: int, block_header, dat_io, b, bm):
+def check_block_merkle(block_header, dat_io):
     block_io = BlockBytesIO(dat_io)
     merkle_root = block_header[36:36 + 32][::-1].hex()
     tx_number = block_io.read_varint()
     txid_list = []
-    prev_index = dat_io.tell()
-    tx_index_list = []
     while True:
         tx = Transaction.from_hex(block_io)
         if tx is None:
             raise BlockParseError(f'{tx_number}')
         else:
             tx_number -= 1
-            now_index = dat_io.tell()
-            tx_index_list.append(TxIndexer(blk_number, prev_index, now_index - prev_index))
-            prev_index = now_index
             txid_list.append(tx.txid())
         if tx_number == 0:
             break
     tx_merkle_root = merkle(txid_list)
     if merkle_root != 'da2b9eb7e8a3619734a17b55c47bdd6fd855b0afa9c7e14e3a164a279e51bba9' and tx_merkle_root != merkle_root:
         raise BlockParseError(f'merkle verify failed')
-    for i in range(len(txid_list)):
-        txid = txid_list[i]
-        tx_index = tx_index_list[i]
-        txid_bytes = bytes.fromhex(txid)
-        b.put(txid_bytes, tx_index.to_bytes())
-        bm.delete(txid_bytes)
 
 
 class BaseIndexer:
@@ -308,24 +271,6 @@ class BlockIndexer(BaseIndexer):
     pass
 
 
-class TxIndexer(BaseIndexer):
-    pass
-
-
-def get_blk_dat_file(blk_number: int):
-    return f'{NODE_DATA_PATH}/blocks/blk{str(blk_number).zfill(5)}.dat'
-
-
-def save_mempool_tx():
-    if not memory_queue.empty():
-        txid_list = []
-        with db_level_mempool.write_batch() as b:
-            while not memory_queue.empty():
-                item = memory_queue.get()
-                txid_list.append(item['txid'])
-                b.put(bytes.fromhex(item['txid']), item['tx_binary'])
-        log("clear memory queue", len(txid_list))
-
 # api
 def get_current_username(
         credentials: Annotated[HTTPBasicCredentials, Depends(security)]
@@ -353,7 +298,6 @@ def get_current_username(
 def obj_info(username: Annotated[str, Depends(get_current_username)], q: Union[str, None] = None):
     log('username:', username)
     data = {
-        'mempool': False,
         'size': 0
     }
     try:
@@ -364,13 +308,7 @@ def obj_info(username: Annotated[str, Depends(get_current_username)], q: Union[s
         v = db_level_indexer.get(q_bytes)
         if v:
             info = BaseIndexer.from_bytes(v)
-            data['mempool'] = False
             data['size'] = info.size
-        else:
-            v = db_level_mempool.get(q_bytes)
-            if v:
-                data['mempool'] = True
-                data['size'] = len(v)
     return data
 
 
@@ -394,27 +332,16 @@ def obj_chunk(username: Annotated[str, Depends(get_current_username)], q: str, c
                 tx_bytes = f.read(read_size)
                 return Response(tx_bytes, media_type='application/x-binary')
         else:
-            v = db_level_mempool.get(q_bytes)
-            if v:
-                f = BytesIO(v)
-                f.seek(chunk_index * chunk_size)
-                tx_bytes = f.read(chunk_size)
-                return Response(tx_bytes, media_type='application/x-binary')
-            else:
-                raise HTTPException(status_code=400, detail=f'${q} not exists')
+            raise HTTPException(status_code=400, detail=f'${q} not exists')
 
 
 def main():
-    # zmq thread
-    zmqThread = ZmqThread(ZMQ_SERVER)
-    zmqThread.start()
     # api thread
     apiThread = ApiThread()
     apiThread.start()
     # main thread
     try:
         while True:
-            save_mempool_tx()
             blk_data = get_blk_dat_file(config["blk_number"])
             blk_next_data = get_blk_dat_file(config["blk_number"] + 1)
             if os.path.exists(blk_data):
@@ -422,12 +349,12 @@ def main():
                     dat_io = DatParse(f, config['position'])
                     while True:
                         magic = dat_io.read_magic()
-                        if magic == b'i\xc3Z\xa5':
+                        if magic == MAGIC_BYTES:
                             # block size
                             block_data_size = dat_io.read_size()
                             # check next
                             re_read_magic = dat_io.check_next_magic()
-                            if re_read_magic == b'i\xc3Z\xa5':
+                            if re_read_magic == MAGIC_BYTES:
                                 continue
                             # block header
                             block_header_start = dat_io.tell()
@@ -435,16 +362,15 @@ def main():
                             block_header = dat_io.read_block_header()
                             block_hash_bytes = hash256(block_header)[::-1]
                             with db_level_indexer.write_batch() as b:
-                                with db_level_mempool.write_batch() as bm:
-                                    b.put(block_hash_bytes, block_index.to_bytes())
-                                    block_hash = block_hash_bytes.hex()
-                                    try:
-                                        upload_block_txs(config['blk_number'], block_header, dat_io, b, bm)
-                                    except BlockParseError as e:
-                                        log(f'mvc node write block not completed {block_hash}')
-                                        log(e)
-                                        break
-                                    log(block_hash, config['blk_number'], dat_io.position, block_index, config)
+                                b.put(block_hash_bytes, block_index.to_bytes())
+                                block_hash = block_hash_bytes.hex()
+                                try:
+                                    check_block_merkle(block_header, dat_io)
+                                except BlockParseError as e:
+                                    log(f'mvc node write block not completed {block_hash}')
+                                    log(e)
+                                    break
+                                log(block_hash, config['blk_number'], dat_io.position, block_index, config)
                         else:
                             # check next block exists, if exists position set 0
                             if os.path.exists(blk_next_data):
@@ -462,25 +388,23 @@ def main():
                                 time.sleep(0.1)
                             break
     except KeyboardInterrupt as _:
+        log('interrupted', _)
         pass
 
 
 # instance config
 config = get_config()
 
-
 if __name__ == '__main__':
     """
     1. main thread listen block and save index to leveldb
-    2. zmq thread listen zmq rawtx cal txid save to leveldb
-    3. use merkle root verify when save block
+    2. use merkle root verify when save block
     Todo:
         1. scan block file <blockID:blk_number:start:size> write to leveldb ok
-        2. scan transaction <txid:blk_number:start:size>   write to leveldb ok
-        3. /obj/info get file size size                                     ok
-        4. /obj/chunk?chunk_index=0&chunk_size=1024000 get file chunk       ok
-        5. add basic auth user password same as node                        ok
-        6. write js promise sdk                                             ok
-        7. test different chunk size speed                                  ok
+        2. /obj/info get file size size                                     ok
+        3. /obj/chunk?chunk_index=0&chunk_size=1024000 get file chunk       ok
+        4. add basic auth user password same as node                        ok
+        5. write js promise sdk                                             ok
+        6. test different chunk size speed                                  ok
     """
     main()
